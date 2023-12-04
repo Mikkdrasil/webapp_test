d238f0de3e95102dc6e899f0f3038c2d7e524605
Mike Trifonov <mike.trifonov@gmail.com>
2023-12-04 13:35:07 +0100

    try to pass data from tg to webapp

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
+const { first_name, last_name, username } = tg.initDataUnsafe.user;
+
+const a = `Сообщение отправлено юзеру ${first_name}, ${last_name}, ${username}`
+
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.setText(`Сообщение отправлено to ${username}`);
     tg.MainButton.show();
     tg.sendData("sendTestMessage");
 });
