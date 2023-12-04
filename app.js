2168d1215cb75303c8478ec109a21ec3b07e50ef
Mike Trifonov <mike.trifonov@gmail.com>
2023-12-04 14:04:26 +0100

    try to pass data from tg to webapp 2

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
-const { first_name, last_name, username } = tg.initDataUnsafe.user;
-
-const a = `Сообщение отправлено юзеру ${first_name}, ${last_name}, ${username}`
-
 let btn = document.getElementById("btn");
 
+const { first_name, last_name, username } = window.Telegram.WebApp.initDataUnsafe.user;
+
+document.getElementById("user_name").innerHTML = username;
+
 btn.addEventListener("click", function(){
-    tg.MainButton.setText(`Сообщение отправлено to ${username}`);
+    tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
     tg.sendData("sendTestMessage");
 });
